24c69821f0ded0b6d082c54c2d37dab41476e7cf
wi1d5ky <poppin.wildsky@gmail.com>
2016-01-27 22:07:12 +0800

    Remoce file extension

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "posts"
 
-  blog.permalink = "posts/{title}.html"
+  blog.permalink = "posts/{title}/index.html"
   # Matcher for blog source files
   blog.sources = "posts/{year}-{month}-{day}-{title}.html"
   blog.taglink = "tags/{tag}.html"
