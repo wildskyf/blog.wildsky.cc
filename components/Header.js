b4b89394acca2ceced1a9c51418de7abaa802820
wildskyf <poppin.wildsky+gh@gmail.com>
2021-03-30 13:36:15 +0800

    minor fix

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-import { useWindowSize } from 'react-use'
 import Link from 'next/link'
 import Nav from '../components/Nav'
 
 const Header = ({ home_url, website_name, website_description, menu_items }) => {
-  const { width } = useWindowSize()
-  const isDesktop = width >= 640
-  const [isNavOpen, setNavOpen] = useState(false)
-
   return (
     <>
       <h1 className='font-title font-semibold lg:font-bold mt-3 mb-3 lg:mb-6 text-3xl lg:text-6xl text-gray-900 hover:text-red-700'>
@@ -17,15 +11,7 @@ const Header = ({ home_url, website_name, website_description, menu_items }) => 
         {website_description}
       </p>
       <nav className='flex flex-col justify-center items-center w-full lg:w-auto select-none'>
-        <div
-          className='inline-block lg:hidden bg-white px-4 py-2 border-2 border-solid border-gray-200'
-          onClick={() => setNavOpen(!isNavOpen)}
-        >
-          Menu
-        </div>
-        {
-          (isDesktop || isNavOpen) && <Nav menu_items={menu_items} />
-        }
+        <Nav menu_items={menu_items} />
       </nav>
     </>
   )
--- components/Nav.js
+++ components/Nav.js
@@ -1,30 +1,50 @@
+import { useState } from 'react'
+import { useWindowSize } from 'react-use'
 import Link from 'next/link'
 
 const Nav = ({ menu_items }) => {
+  const { width } = useWindowSize()
+  const isDesktop = width >= 640
+  const [isNavOpen, setNavOpen] = useState(false)
+
   // TODO: i18n
   const items = menu_items.filter(item => {
     return !['Languages'].includes(item.title)
   })
 
+  const toggleNav = () => setNavOpen(!isNavOpen)
+
   return (
-    <ul className="flex flex-col text-center lg:flex-row w-full lg:w-auto">
+    <>
+      <div
+        className='inline-block lg:hidden bg-white px-4 py-2 border-2 border-solid border-gray-200'
+        onClick={toggleNav}
+      >
+        Menu
+      </div>
       {
-        items.map(item => {
-          return (
-            <li key={item.guid}>
-              <Link href={item.url}>
-                <a className={`
-                  text-gray-700 text-xl font-semibold px-3 py-3 mx-1 cursor-pointer hover:text-red-700 transition-colors w-full block
-                  border-b-2 border-gray-200 border-solid
-                `}>
-                  {item.title}
-                </a>
-              </Link>
-            </li>
-          )
-        })
+        (isDesktop || isNavOpen) && (
+          <ul className="flex flex-col text-center lg:flex-row w-full lg:w-auto" onClick={toggleNav}>
+            {
+              items.map(item => {
+                return (
+                  <li key={item.guid}>
+                    <Link href={item.url}>
+                      <a className={`
+                        text-gray-700 text-xl font-semibold px-3 py-3 mx-1 cursor-pointer hover:text-red-700 transition-colors w-full block
+                        border-b-2 border-gray-200 border-solid
+                      `}>
+                        {item.title}
+                      </a>
+                    </Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        )
       }
-    </ul>
+    </>
   )
 }
 
