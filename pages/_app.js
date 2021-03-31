d3881a11e834c1db9b0f5550dd46702a8523ead4
wildskyf <poppin.wildsky+gh@gmail.com>
2021-03-31 12:13:37 +0800

    add ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
+import Router from "next/router"
+import withGA from "next-ga"
+
 import '../global.css'
 import 'tailwindcss/tailwind.css'
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
+export default withGA('UA-50389098-1', Router)(MyApp)
