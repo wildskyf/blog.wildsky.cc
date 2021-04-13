46a808069fb122a28167e5e96e528415a06fca16
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 10:44:01 +0800

    refactor page

--- pages/[page_slug].js
+++ pages/[page_slug].js
@@ -5,13 +5,15 @@ import Article from '../components/Article'
 import fetchPageShowPath from '../libs/fetchPageShowPath'
 import fetchPageShowData from '../libs/fetchPageShowData'
 
-export const PageComponent = (props) => {
+const LANG = 'tw'
+
+export const PageComponentFactory = lang => (props) => {
   const { pageName, pageDesc } = props
   const { website_name, website_description, home_url, menu_items } = props
   const { page_feature_image_url, page_title, page_content } = props
 
   return (
-    <Layout {...{ lang: 'tw', home_url, website_name, website_description, menu_items }}>
+    <Layout {...{ lang, home_url, website_name, website_description, menu_items }}>
       <Head  {...{ pageName, pageDesc }} />
       <Article
         feature_image_url={page_feature_image_url}
@@ -22,26 +24,27 @@ export const PageComponent = (props) => {
   )
 }
 
+export const getStaticPathsFactory = lang => async () => {
+  const paths = await fetchPageShowPath({ lang })
+  return { paths, fallback: false }
+}
+
+export const getStaticPropsFactory = lang => async ({ params }) => {
+  const ret_props = await fetchPageShowData({ page_slug: params.page_slug, lang })
+  return { props: ret_props }
+}
+
 const Page = (props) => {
+  const PageComponent = PageComponentFactory(LANG)
   return (
     <PageComponent {...props}
-      lang='tw'
+      lang={LANG}
       pageName={props.page_title}
       pageDesc={props.page_content && props.page_content.substring(0, 70)}
     />
   )
 }
 
-export const getStaticPaths = async () => {
-  const paths = await fetchPageShowPath({ lang: 'tw' })
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }) => {
-  const ret_props = await fetchPageShowData({ page_slug: params.page_slug, lang: 'tw' })
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default Page
--- pages/en/[page_slug].js
+++ pages/en/[page_slug].js
@@ -1,27 +1,18 @@
-import { PageComponent } from '../[page_slug]'
-import fetchPageShowPath from '../../libs/fetchPageShowPath'
-import fetchPageShowData from '../../libs/fetchPageShowData'
+import { PageComponentFactory, getStaticPathsFactory, getStaticPropsFactory } from '../[page_slug]'
+
+const LANG = 'en'
 
 const Page = (props) => {
+  const PageComponent = PageComponentFactory(LANG)
   return (
     <PageComponent {...props}
-      lang='en'
+      lang={LANG}
       pageName={props.page_title}
       pageDesc={props.page_content && props.page_content.substring(0, 70)}
     />
   )
 }
 
-export const getStaticPaths = async () => {
-  const paths = await fetchPageShowPath({ lang: 'en' })
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }) => {
-  const ret_props = await fetchPageShowData({ page_slug: params.page_slug, lang: 'en' })
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default Page
