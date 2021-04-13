02b20e8e4d062994962cd9aee98d0655e4991edb
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 14:01:26 +0800

    sticky footer

--- layout/page.js
+++ layout/page.js
@@ -5,12 +5,12 @@ const PageLayout = ({ lang, home_url, website_name, website_description, menu_it
   const lang_code = lang === 'tw' ? 'zh-Hant-TW' : 'en'
 
   return (
-    <div lang={lang_code}>
+    <div className='min-h-screen flex flex-col'>
       <header className='flex flex-col justify-center items-center px-4 py-12 bg-gray-100 border-b-2 border-gray-200 border-solid'>
         <Header {...{ lang, home_url, website_name, website_description, menu_items }} />
       </header>
 
-      <main className='flex justify-center items-center py-4' lang={lang_code}>
+      <main className='flex-grow flex justify-center items-start py-4' lang={lang_code}>
         <div className='flex flex-col w-full max-w-full lg:max-w-3xl'>
           {children}
         </div>
