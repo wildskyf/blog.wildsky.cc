d10e3a5285446f3f56420567201d06d3ec06029f
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 13:24:50 +0800

    update lang info

--- layout/page.js
+++ layout/page.js
@@ -1,22 +1,24 @@
 import Header from '../components/Header'
 
 const PageLayout = ({ lang, home_url, website_name, website_description, menu_items, children }) => {
+  const lang_code = lang === 'tw' ? 'zh-Hant-TW' : 'en'
+
   return (
-    <>
+    <div lang={lang_code}>
       <header className='flex flex-col justify-center items-center px-4 py-12 bg-gray-100 border-b-2 border-gray-200 border-solid'>
         <Header {...{ lang, home_url, website_name, website_description, menu_items }} />
       </header>
 
-      <main className='flex justify-center items-center py-4' lang={lang === 'tw' ? 'zh-Hant-TW' : 'en'}>
+      <main className='flex justify-center items-center py-4' lang={lang_code}>
         <div className='flex flex-col w-full max-w-full lg:max-w-3xl'>
           {children}
         </div>
       </main>
 
-      <footer className='bg-gray-800 text-white text-center py-16'>
+      <footer className='bg-gray-800 text-white text-center py-16' lang='en'>
         Geng Zhi W. Fann
       </footer>
-    </>
+    </div>
   )
 }
 
