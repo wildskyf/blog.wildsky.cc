d17f571e982d940099d125aaedd7103c77b7fe38
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 10:49:55 +0800

    refactor pagination

--- pages/en/page/[page_num].js
+++ pages/en/page/[page_num].js
@@ -1,38 +1,19 @@
-import { PaginationPageComponent } from '../../page/[page_num]'
+import { PaginationPageComponentFactory, getStaticPathsFactory, getStaticPropsFactory } from '../../page/[page_num]'
 
-import fetchPaginationShowPath from '../../../libs/fetchPaginationShowPath'
-import fetchPaginationShowData from '../../../libs/fetchPaginationShowData'
+const LANG = 'en'
 
 const EnPaginationPage = (props) => {
+  const PaginationPageComponent = PaginationPageComponentFactory(LANG)
+
   return (
     <PaginationPageComponent {...props}
-      lang='en'
+      lang={LANG}
       pageName={`Article List Page ${props.current_page_num}`}
       pageDesc='Hi I am Geng-Zhi, using Wildsky as my codename on the Internet. Currently working in Taiwan AI Labs, and I am a keyholder in Taiwan Mozilla Community. With this blog, I hope to find the answers for three big questions: who am I, Where am I from, and Where to go.'
     />
   )
 }
 
-export const getStaticPaths = async () => {
-  const paths = await fetchPaginationShowPath({ lang: 'en' })
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }) => {
-  if (params.page_num === 1) {
-    return {
-      redirect: {
-        destination: '/en',
-        permanent: true
-      }
-    }
-  }
-
-  const ret_props = await fetchPaginationShowData({ page_num: params.page_num, lang: 'en' })
-
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default EnPaginationPage
--- pages/page/[page_num].js
+++ pages/page/[page_num].js
@@ -3,8 +3,8 @@ import Layout from '../../layout/list'
 import fetchPaginationShowPath from '../../libs/fetchPaginationShowPath'
 import fetchPaginationShowData from '../../libs/fetchPaginationShowData'
 
-export const PaginationPageComponent = (props) => {
-  const { lang, pageName, pageDesc } = props
+export const PaginationPageComponentFactory = lang => (props) => {
+  const { pageName, pageDesc } = props
   const { website_name, website_description, home_url, menu_items } = props
   const { postList, current_page_num, total_page_num } = props
   const path_prefix = lang === 'en' ? '/en' : ''
@@ -25,39 +25,35 @@ export const PaginationPageComponent = (props) => {
       }} />
     </>
   )
-
 }
 
+export const getStaticPathsFactory = lang => async () => {
+  const paths = await fetchPaginationShowPath({ lang })
+  return { paths, fallback: false }
+}
+
+export const getStaticPropsFactory = lang => async ({ params }) => {
+  if (params.page_num === 1) {
+    const destination = lang === 'en' ? '/en' : '/'
+    return { redirect: { destination, permanent: true } }
+  }
+  const ret_props = await fetchPaginationShowData({ page_num: params.page_num, lang })
+  return { props: ret_props }
+}
+
+const LANG = 'tw'
+
 const PaginationPage = (props) => {
+  const PaginationPageComponent = PaginationPageComponentFactory(LANG)
   return (
     <PaginationPageComponent {...props}
-      lang='tw'
+      lang={LANG}
       pageName={`文章列表 第${props.current_page_num}頁`}
       pageDesc='我是耿誌，網路上的代號是 Wildsky 。會開始寫部落格，首要原因是希望能記下遇過的問題或學會的新技能，也想著或許能幫助到哪個網路上陌生人。'
     />
   )
 }
 
-export const getStaticPaths = async () => {
-  const paths = await fetchPaginationShowPath({ lang: 'tw' })
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }) => {
-  if (params.page_num === 1) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: true
-      }
-    }
-  }
-
-  const ret_props = await fetchPaginationShowData({ page_num: params.page_num, lang: 'tw' })
-
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default PaginationPage
