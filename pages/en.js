db652343fea472a5c81cc1de9f4004faa161b7b4
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 11:12:30 +0800

    refactor index

--- pages/en.js
+++ pages/en.js
@@ -1,22 +1,18 @@
-import { HomeComponent } from './index'
-import fetchIndexData from '../libs/fetchIndexData'
+import { HomeComponentFactory, getStaticPropsFactory } from './index'
+
+const LANG = 'en'
 
 const EnHome = (props) => {
+  const HomeComponent = HomeComponentFactory(LANG)
+
   return (
     <HomeComponent {...props}
-      lang='en'
+      lang={LANG}
       pageName='Home'
       pageDesc='Hi I am Geng-Zhi, using Wildsky as my codename on the Internet. Currently working in Taiwan AI Labs, and I am a keyholder in Taiwan Mozilla Community. With this blog, I hope to find the answers for three big questions: who am I, Where am I from, and Where to go.'
     />
   )
 }
 
-export const getStaticProps = async () => {
-  const ret_props = await fetchIndexData({ lang: 'en' })
-
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default EnHome
--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import Layout from '../layout/list'
 import Head from '../components/Head'
 import fetchIndexData from '../libs/fetchIndexData'
 
-export const HomeComponent = (props) => {
-  const { lang, pageName, pageDesc } = props
+const LANG = 'tw'
+
+export const HomeComponentFactory = lang => (props) => {
+  const { pageName, pageDesc } = props
   const path_prefix = lang === 'en' ? '/en' : ''
 
   return (
@@ -14,22 +16,21 @@ export const HomeComponent = (props) => {
   )
 }
 
+export const getStaticPropsFactory = lang => async () => {
+  const ret_props = await fetchIndexData({ lang })
+  return { props: ret_props }
+}
+
 const Home = (props) => {
+  const HomeComponent = HomeComponentFactory(LANG)
   return (
     <HomeComponent {...props}
-      lang='tw'
+      lang={LANG}
       pageName='首頁'
       pageDesc='我是耿誌，網路上的代號是 Wildsky 。會開始寫部落格，首要原因是希望能記下遇過的問題或學會的新技能，也想著或許能幫助到哪個網路上陌生人。'
     />
   )
 }
 
-export const getStaticProps = async () => {
-  const ret_props = await fetchIndexData({ lang: 'tw' })
-
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default Home
