c953b86c73b792e790ba926da3c60726d40fad45
wildskyf <poppin.wildsky+gh@gmail.com>
2021-04-13 11:09:54 +0800

    refactor post

--- pages/en/posts/[post_slug].js
+++ pages/en/posts/[post_slug].js
@@ -1,29 +1,18 @@
-import { PostComponent } from '../../posts/[post_slug]'
+import { PostComponentFactory, getStaticPathsFactory, getStaticPropsFactory } from '../../posts/[post_slug]'
 
-import fetchPostShowPath from '../../../libs/fetchPostShowPath'
-import fetchPostShowData from '../../../libs/fetchPostShowData'
+const LANG = 'en'
 
 const Post = (props) => {
+  const PostComponent = PostComponentFactory(LANG)
   return (
     <PostComponent {...props}
-      lang='en'
+      lang={LANG}
       pageName={props.post_title}
       pageDesc={props.pageDesc}
     />
   )
 }
 
-export const getStaticPaths = async () => {
-  const paths = await fetchPostShowPath({ lang: 'en' })
-  return { paths, fallback: false }
-}
-
-export const getStaticProps = async ({ params }) => {
-  const ret_props = await fetchPostShowData({ post_slug: params.post_slug, lang: 'en' })
-
-  return {
-    props: ret_props
-  }
-}
-
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default Post
--- pages/posts/[post_slug].js
+++ pages/posts/[post_slug].js
@@ -5,7 +5,9 @@ import Article from '../../components/Article'
 import fetchPostShowPath from '../../libs/fetchPostShowPath'
 import fetchPostShowData from '../../libs/fetchPostShowData'
 
-export const PostComponent = (props) => {
+const LANG = 'tw'
+
+export const PostComponentFactory = lang => (props) => {
   const { pageName, pageDesc } = props
   const { website_name, website_description, home_url, menu_items } = props
   const { post_feature_image_url, post_date, post_title, post_content } = props
@@ -13,7 +15,7 @@ export const PostComponent = (props) => {
   return (
     <>
       <Head {...{ pageName, pageDesc }} />
-      <Layout {...{ home_url, website_name, website_description, menu_items }}>
+      <Layout {...{ lang, home_url, website_name, website_description, menu_items }}>
         <Article
           feature_image_url={post_feature_image_url}
           date={post_date}
@@ -25,27 +27,31 @@ export const PostComponent = (props) => {
   )
 }
 
-const Post = (props) => {
-  return (
-    <PostComponent {...props}
-      lang='tw'
-      pageName={props.post_title}
-      pageDesc={props.pageDesc}
-    />
-  )
-}
-
-export const getStaticPaths = async () => {
-  const paths = await fetchPostShowPath({ lang: 'tw' })
+export const getStaticPathsFactory = lang => async () => {
+  const paths = await fetchPostShowPath({ lang })
   return { paths, fallback: false }
 }
 
-export const getStaticProps = async ({ params }) => {
-  const ret_props = await fetchPostShowData({ post_slug: params.post_slug, lang: 'tw' })
+export const getStaticPropsFactory = lang => async ({ params }) => {
+  const ret_props = await fetchPostShowData({ post_slug: params.post_slug, lang })
 
   return {
     props: ret_props
   }
 }
 
+
+const Post = (props) => {
+  const PostComponent = PostComponentFactory(LANG)
+  return (
+    <PostComponent {...props}
+      lang={LANG}
+      pageName={props.post_title}
+      pageDesc={props.pageDesc}
+    />
+  )
+}
+
+export const getStaticPaths = getStaticPathsFactory(LANG)
+export const getStaticProps = getStaticPropsFactory(LANG)
 export default Post
