ffa8b916b092fd3a2af1dd7ac26608b645ffb089
wildskyf <poppin.wildsky+gh@gmail.com>
2022-04-16 14:10:03 +0800

    typo

--- libs/fetchPostShowPath.js
+++ libs/fetchPostShowPath.js
@@ -3,7 +3,7 @@ import fs from 'fs'
 const fetchPostShowData = async ({ lang }) => {
   const post_filenames = fs
     .readdirSync(`./data/posts-${lang}/`)
-    .map(f => f.repalce(/.json$/, ''))
+    .map(f => f.replace(/.json$/, ''))
 
   const ret = post_filenames.map((post) => ({
     params: { post_slug: post },
