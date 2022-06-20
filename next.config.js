d15d2f3cae1a49e70ce81bea554ae7dcccc38b30
wildskyf <poppin.wildsky+gh@gmail.com>
2022-06-20 17:52:40 +0800

    upgrade next.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ module.exports = () => {
     webpack: (config, { isServer }) => {
       // Fixes npm packages that depend on `fs` module
       if (!isServer) {
-        config.node = { fs: 'empty' }
+        config.resolve.fallback = {
+          ...config.resolve.fallback,
+          fs: false
+        }
       }
 
       config.module.rules.push({
