236fdbbaeddb83f7d60b200904be13b123e39f2d
wildskyf <poppin.wildsky+gh@gmail.com>
2022-07-15 21:42:43 +0800

    Bugfix: fix style of 404 page

--- pages/404.js
+++ pages/404.js
@@ -6,23 +6,32 @@ import Button from '../components/Button'
 const LANG = 'en'
 
 const NotFoundPage = (props) => {
-  const pageName = '404 NotFound'
+  const errorCode = '404'
+  const pageName = 'Not Found'
   const pageDesc = "That page can't be found."
   return (
     <>
       <Head {...{ pageName, pageDesc }} />
       <Layout { ...{ lang: LANG, ...props } }>
-        <div className='flex justify-center items-center'>
-          <div className='py-24 px-6 lg:px-0 lg:pl-48 w-full max-w-xl lg:transform lg:-rotate-90'>
-            <h1 className='text-5xl font-bold text-gray-800'>
-              { pageName }
-            </h1>
-            <p className='mb-8 inline-block text-5xl text-gray-300 whitespace-nowrap origin-bottom-left transform rotate-90'>{ pageDesc }</p>
+
+        <div className='flex flex-col justify-center border p-8'>
+          <div className='flex justify-center gap-x-2'>
+            <div className='text-9xl cursor-default duration-200 text-red-100 hover:text-red-700'>
+              {errorCode}
+            </div>
+
             <Button
               to='/'
-              className='text-6xl uppercase text-right py-36 px-8 font-bold'
-              text={<>back<br/>to<br/>home</>}
+              className='text-4xl duration-200 opacity-20 hover:opacity-100'
+              text={<>Back<br/ >To<br />Home</>}
             />
+          </div>
+
+          <div className={`
+            flex justify-center text-9xl cursor-default duration-200
+            text-gray-200 hover:text-gray-700
+          `}>
+            {pageName}
           </div>
         </div>
       </Layout>
