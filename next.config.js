63df9c707133129496646db53bd9c3f87ed75080
wildskyf <poppin.wildsky+gh@gmail.com>
2022-07-22 21:46:05 +0800

    Feature: add syntax highlight for articles

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+const markdownIt = require('markdown-it');
+const markdownItPrism = require('markdown-it-prism');
+
 module.exports = () => {
   return {
     poweredByHeader: false,
@@ -7,7 +10,13 @@ module.exports = () => {
 
       config.module.rules.push({
         test: /\.md$/,
-        use: 'frontmatter-markdown-loader'
+        use: [{
+          loader: 'frontmatter-markdown-loader',
+          options: {
+            markdownIt: markdownIt({ html: true }).use(markdownItPrism),
+          }
+        }]
+
       })
 
       return config
