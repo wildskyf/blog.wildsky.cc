cdd9d310744cc1cbf76d9b50adbb7af85c543be7
wildskyf <poppin.wildsky+gh@gmail.com>
2023-05-02 15:13:48 +0800

    specify timezone

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,11 @@
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
 import Router from "next/router"
 import withGA from "next-ga"
-
 import '../global.css'
+
+dayjs.extend(timezone)
+dayjs.tz.setDefault('Asia/Taipei')
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
