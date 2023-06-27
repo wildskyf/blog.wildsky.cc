216656b4662ff9d02608d34ca649a4b1ca1bd3c7
wildskyf <poppin.wildsky+gh@gmail.com>
2023-06-27 15:04:49 +0800

    replace next-ga with next/script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,28 @@
+import Script from 'next/script'
 import dayjs from 'dayjs'
 import timezone from 'dayjs/plugin/timezone'
 import Router from "next/router"
-import withGA from "next-ga"
 import '../global.css'
 
 dayjs.extend(timezone)
 dayjs.tz.setDefault('Asia/Taipei')
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const GA_ID = 'G-QJC649ZTNK'
 
-export default withGA('G-QJC649ZTNK', Router)(MyApp)
+export default ({ Component, pageProps }) => {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${GA_ID});
+        `}
+      </Script>
+    </>
+  )
+}
