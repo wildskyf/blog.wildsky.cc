18d4aedc2bd70c38b5ea5c0e0a5f62831355f249
wildskyf <poppin.wildsky+gh@gmail.com>
2024-04-30 22:35:23 +0800

    extract articleMapper into utils

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
+import { base_url } from '@/data/blog_info'
+
 export const jsonify = r => r.json()
+
+export const articleMapper = post_info => {
+  return {
+    title: post_info.data.title,
+    slug: post_info.data.slug,
+    date: post_info.data.date,
+    guid: post_info.data.guid,
+    abs_feature_image: `${base_url}/images/${post_info.data.feature_image}`,
+    feature_image_url: post_info.data.feature_image ? `/images/${post_info.data.feature_image}` : '',
+    excerpt: post_info.data.excerpt,
+  }
+}
