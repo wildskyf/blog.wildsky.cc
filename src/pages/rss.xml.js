c251dfb47db1dd324ed23544dd1915139657415c
wildskyf <poppin.wildsky+gh@gmail.com>
2025-03-09 17:15:52 +0800

    fix rss content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,18 @@
 import rss, { pagesGlobToRssItems } from '@astrojs/rss'
+import { getCollection } from 'astro:content';
+
 
 export const GET = async (context) => {
+  const postsTw = getCollection('posts-tw')
+  const postsEn = getCollection('posts-en')
+
   return rss({
     title: "Wildsky's Blog",
     description: 'Dev blog about server hosting, web dev, and some of my daily stuffs.',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: [
+      ...postsTw,
+      ...postsEn
+    ]
   })
 }
